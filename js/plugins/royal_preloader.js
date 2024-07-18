3bbde65350ad6eb5a01b61803cdc3d1c7533ddb5
malembi <samirmaleymbi@yahoo.com>
2024-07-19 01:20:18 +0300

    loader color

--- js/plugins/royal_preloader.js
+++ js/plugins/royal_preloader.js
@@ -89,7 +89,7 @@ var Royal_Preloader = {
             this._text_loader_overlay = $("<div>").css("background-color", this._background[0]).appendTo(this._text_loader);
         } else {
             this._logo_loader = $("<div>").css("background-image", 'url("images/nwlogo.png")').addClass("loader").appendTo(this._overlay);
-            this._logo_loader_meter = $("<div>").css("background-color", this._background[0]).appendTo(this._logo_loader);
+            this._logo_loader_meter = $("<div>").css("background-color", this._background[0]);
             this._percentage = $("<div>").css("background-color", this._background[0]).addClass("percentage").appendTo(this._overlay).hide(); // Hide percentage
         }
         
