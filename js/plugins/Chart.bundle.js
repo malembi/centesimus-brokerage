0c45cf87387c7eeecb66423978a4f01ed3f101de
faima <faima.mwami@cellulant.io>
2024-07-18 23:16:48 +0300

    fix

--- js/plugins/Chart.bundle.js
+++ js/plugins/Chart.bundle.js
@@ -2324,7 +2324,7 @@ module.exports = {
     var matchOffset    = /Z|[+-]\d\d:?\d\d/gi; // +00:00 -00:00 +0000 -0000 or Z
     var matchShortOffset = /Z|[+-]\d\d(?::?\d\d)?/gi; // +00 -00 +00:00 -00:00 +0000 -0000 or Z
 
-    var matchTimestamp = /[+-]?\d+(\.\d{1,3})?/; // 123456789 123456789.123
+    var matchTimestamp = /[+-]?\d+(\.\d{1,3})?/; // 462862529 462862529.123
 
     // any word (or two) characters or numbers including two/three word month in arabic.
     // includes scottish gaelic two word and hyphenated months
